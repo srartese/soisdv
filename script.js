f276aa0c18db439199658c49fa3f2bd01e67f546
srartese <saa2457@rit.edu>
2017-03-04 18:24:29 -0500

    Implemented options and processing json file.

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
- 	"use strict"; 
+/* SOIS Data Visualization using Google Charts
+	By Matthew Graham and Sara Artese
+	Last Updated: March 4, 2017
+*/
+
+
+(function(){
+ 	"use strict";
  	
  	google.charts.load('current', {'packages':['corechart']});
     google.charts.setOnLoadCallback(drawSeriesChart);
 
     function drawSeriesChart() {
-
-      var data = google.visualization.arrayToDataTable([
-        ["ID",      "Date",     "", "", "Enrollment"],
-        ["1887-88", 1887,       0,  510,  510],
-        ["1888-89", 1888,       0,  454,  454],
-        ["1889-90", 1889,       0,  798,  798],
-        ["1890-91", 1890,       0,  711,  711],
-        ["1891-92", 1891,       0,  721,  721],
-        ["1892-93", 1892,       0,  736,  736],
-        ["1893-94", 1893,       0,  753,  753],
-      ]);
-
-      
+	
+    var jsonData = "SIOSdata.json";
+    
+	var data = new google.visualization.DataTable(jsonData);
+	
+      // Opaque until on hover ?
 
       var options = {
-        title: 'Correlation between life expectancy, fertility rate ' +
-               'and population of some world countries (2010)',
-        hAxis: {title: 'Date'},
-		    vAxis: {title: ''},
-        bubble: {textStyle: {fontSize: 11}}
+        title: 'RIT School of Individualized Studies Enrollment from 1885-2017',
+        hAxis: {title: 'Year',format: ''},
+		vAxis: {title: '', format: ''},
+        bubble: {textStyle: {fontSize: 11}},
+        colorAxis: {colors:['#FF3333','#8133FF']},
+        sizeAxis: {minValue: 0,  maxSize: 50}
       };
 
       var chart = new google.visualization.BubbleChart(document.getElementById('series_chart_div'));
       chart.draw(data, options);
     }
+    
+	//window.addEventListener("load",init);
+}());
