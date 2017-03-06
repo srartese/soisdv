9030cdf2181236ef7c0476036c1b057c58590b57
matthewgraham <mpgraham917@gmail.com>
2017-03-05 21:33:35 -0500

    THERE IT WORKS

--- script.js
+++ script.js
@@ -12,11 +12,17 @@
   google.charts.setOnLoadCallback(drawSeriesChart);
 
   function drawSeriesChart() {
-	  
-	document.querySelector('#fiveCheckbox').onchange = function(e){
-		byFive = e.target.checked;
-	}
-	console.log(byFive);
+    
+  	document.querySelector('#fiveCheckbox').onchange = function(e){
+  		byFive = e.target.checked;
+  	}
+  	console.log(byFive);
+
+    var parabolaBtn = document.getElementById('parabolaBtn');
+    parabolaBtn.onclick = function(){
+      drawParabolaChart();
+    };
+
     var weightedSOISdata = [
       ["ID", "Date", "", "", "Enrollment"],
     ];
@@ -57,7 +63,12 @@
       explorer:{
         axis: 'horizontal',
         maxZoomIn: 1,
-        maxZoomOut: 8
+        maxZoomOut: 8,
+        zoomDelta: 1.1
+      },
+      animation:{
+        duration: 1000,
+        easing: 'out',
       },
       tooltip: {trigger: 'none'}
     };
@@ -74,8 +85,67 @@
 	  }
   }
   
-  function loadNewMap(){
+  function drawParabolaChart(){
+    var chart = new google.visualization.BubbleChart(document.getElementById('series_chart_div'));
+    // Disabling the button while the chart is drawing.
+    parabolaBtn.disabled = true;
+    google.visualization.events.addListener(chart, 'ready',
+        function() {
+          parabolaBtn.disabled = false;
+        });
 
+    var parabolaSOISdata = [
+      ["ID", "Year", "Enrollment", "Size"]
+    ];
+    for(var i = 1; i < SOISdata.length; i++){
+      var d = SOISdata[i];
+      parabolaSOISdata.push([
+        d[0].slice(0,4), d[1], d[3], d[3]
+      ]);
+    }
+
+    var data = new google.visualization.arrayToDataTable(parabolaSOISdata);
+
+    var options = {
+      title: 'RIT School of Individualized Studies Enrollment from 1885-2017',
+      hAxis: {
+        title: '', 
+        format: '', 
+        gridlines: {count: 5},
+        minorGridlines: {count:9},
+        viewWindowMode:'explicit',
+        viewWindow:{
+          max:1930,
+          min:1880
+        }
+      },
+      vAxis: {
+        title: '',
+        format: '',
+        gridlines: {count: 10},
+        viewWindowMode:'explicit',
+        viewWindow:{
+          max:8000,
+          min:0
+        }
+      },
+      bubble: {textStyle: {fontSize: 11}},
+      colorAxis: {colors:['#FFFF00','#FF0000']},
+      sizeAxis: {minValue: 0, maxSize: 5},
+      tooltip: {trigger: 'none'},
+      explorer:{
+        axis: 'horizontal',
+        maxZoomIn: 1,
+        maxZoomOut: 4,
+        zoomDelta: 1.1
+      },
+      animation:{
+        duration: 1000,
+        easing: 'out',
+      },
+    };
+
+    chart.draw(data, options);
   }
   
   function byFive(){
@@ -93,10 +163,7 @@
     }
 
     var data = new google.visualization.arrayToDataTable(fiveSOISdata);
-
-
   }
   
-
   window.addEventListener("load", drawSeriesChart);
 }());
