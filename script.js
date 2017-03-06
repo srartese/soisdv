9a6e39d44e769f0a1de791d3ab62b9f73d3dc489
matthewgraham <mpgraham917@gmail.com>
2017-03-05 20:46:09 -0500

    Removed comments

--- script.js
+++ script.js
@@ -27,19 +27,6 @@
 
     var data = new google.visualization.arrayToDataTable(weightedSOISdata);
 
-    /*var testSOISdata = [
-      ["ID", "Date", "", "", "Enrollment"],
-      ["1", 324, 0, 600, 360000],
-      ["2", 1296, 0, 1200, 1440000],
-      ["3", 3240, 0, 2400, 5760000],
-      ["4", 7128, 0, 4800, 23040000],
-      ["5", 14912, 0, 9600, 92160000],
-      ["6", 22688, 0, 4800, 23040000]
-    ];
-    var totalEnrollment = 0;*/
-
-    //var data = new google.visualization.arrayToDataTable(testSOISdata);
-  	
     // Opaque until on hover ?
 
     var options = {
@@ -65,15 +52,18 @@
       explorer:{
         axis: 'horizontal',
         maxZoomIn: 1,
-        maxZoomOut: 1
+        maxZoomOut: 8
       },
       tooltip: {trigger: 'none'}
     };
 
     var chart = new google.visualization.BubbleChart(document.getElementById('series_chart_div'));
-      chart.draw(data, options);
-    }
-    
-	window.addEventListener("load", drawSeriesChart);
+    chart.draw(data, options);
+  }
+  
+  function loadNewMap(){
 
+  }
+
+  window.addEventListener("load", drawSeriesChart);
 }());
